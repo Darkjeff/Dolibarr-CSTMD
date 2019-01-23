32947187116d9be383c11b73f457fc02cf0895dc
Darkjeff <jeff@jeffinfo.com>
2019-01-23 15:59:38 +0100

    corrext mise en forme

--- icstmd/interv_cstmd/data.php
+++ icstmd/interv_cstmd/data.php
@@ -57,7 +57,7 @@ $data = array(
 			"b"=>0,
 			"l"=>"t",
 			"t"=>"",
-			"s"=>"12"
+			"s"=>"10"
 		)
 	),
 	array(
@@ -467,23 +467,23 @@ $page3 = array(
 
 $page4 = array(
 	array('c'=>utf8_decode("1. 1. Organisation de l'entreprise pour les activités liées au transport de marchandises dangereuses"), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode("1.1. Gestion administrative et opérationnelle des activités liées au transport"), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode("1.1. Gestion administrative et opérationnelle des activités liées au transport"), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
 	array('c'=>utf8_decode("1.1.1 Activité de l'entreprise"), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($activiteclient), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode("1.1.2 Organisation et équipements"), 'h'=>8, 's'=>16, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($orgaclient), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
+	array('c'=>$activiteclient, 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode("1.1.2 Organisation et équipements"), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
+	array('c'=>utf8_decode($orgaclient), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
 	array('c'=>utf8_decode("1.2. Place du conseiller à la sécurité dans l'organisation"), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
 	array('c'=>utf8_decode("1.2.1 Interlocuteur du Conseiller à la sécurité ou Organigramme"), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode("L'entreprise dispose d'un conseiller à la sécurité externe toutes classes en relation avec"), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
-	array('c'=>utf8_decode($prenomuser), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($modeuser), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($classuser), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode("Ce conseiller à la sécurité est lié par un contrat et déclaré auprès de la DREAL"), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
-	array('c'=>utf8_decode($drealnom), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($drealaddress), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
-	array('c'=>utf8_decode($drealcpville), 'h'=>8, 's'=>12, 't'=>"B" ,'ln'=>"" ),
+	array('c'=>utf8_decode("L'entreprise dispose d'un conseiller à la sécurité externe toutes classes en relation avec"), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($prenomuser), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($modeuser), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($classuser), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode("Ce conseiller à la sécurité est lié par un contrat et déclaré auprès de la DREAL"), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($drealnom), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($drealaddress), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($drealcpville), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
 	array('c'=>utf8_decode("1.2.2 Organisation par rapport à la sûreté"), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
-	array('c'=>utf8_decode($suretehrtxt), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
+	array('c'=>utf8_decode($suretehrtxt), 'h'=>8, 's'=>10, 't'=>"" ,'ln'=>"" ),
 	array('c'=>utf8_decode("2. Relevé des activités de l'année écoulée"), 'h'=>8, 's'=>12, 't'=>"" ,'ln'=>"" ),
 );
 
